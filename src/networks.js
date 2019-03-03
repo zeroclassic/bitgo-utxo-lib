8b7cdd391a948e5e70c7e5fa0ae1c46e16c11414
Miodrag Popović <miodragpopovic@stcable.net>
2019-03-03 19:43:22 +0100

    ZeroClassic specific

--- src/networks.js
+++ src/networks.js
@@ -22,6 +22,27 @@ module.exports = {
     coin: coins.DEFAULT,
     isZcash: true
   },
+  zerc: {
+    messagePrefix: '\x18Zero Signed Message:\n',
+    bech32: 'bc',
+    bip32: {
+      public: 0x0488b21e,
+      private: 0x0488ade4
+    },
+    pubKeyHash: 0x1cb8,
+    scriptHash: 0x1cbd,
+    wif: 0x80,
+    // This parameter was introduced in version 3 to allow soft forks, for version 1 and 2 transactions we add a
+    // dummy value.
+    consensusBranchId: {
+      1: 0x00,
+      2: 0x00,
+      3: 0x5c7b7d2f,
+      4: 0x7a737763
+    },
+    coin: coins.ZERC,
+    isZcash: true
+  },
   dash: {
     messagePrefix: '\x19DarkCoin Signed Message:\n',
     bip32: {
